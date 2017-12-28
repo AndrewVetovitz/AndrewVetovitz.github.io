e00ee57bf2d82936032ea285e406ecee6f8644ea
AndrewVetovitz <AndrewVetovitz@gmail.com>
2017-12-28 00:52:14 -0500

    added an icon to browser bar, fixed page height issues, got basic content pallette down for job history

--- frontend/src/app/work/work.component.ts
+++ frontend/src/app/work/work.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { WORKDATA } from './work-data';
+
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
   styleUrls: ['./work.component.scss']
 })
 export class WorkComponent implements OnInit {
+  workdata = WORKDATA;
 
   constructor() { }
 
