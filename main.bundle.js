a1b5c6832fd9068d9f127c4cc1e186b1ba35ca99
AndrewVetovitz <AndrewVetovitz@gmail.com>
2018-01-03 15:47:37 -0500

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -793,14 +793,14 @@ var ProjectsComponent = (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return WORKDATA; });
 var WORKDATA = [
     { name: "Goldman Sachs",
-        url: "../../../assets/images/goldman-sachs_logo.jpg",
+        url: "./../../../assets/images/goldman-sachs_logo.jpg",
         alttext: "Goldman Sachs Logo",
         title: "Summer Anaylst Intern, Summer 2018",
         location: "New York, NY",
         link: "http://www.goldmansachs.com/",
         description: "Incoming 2018 Summer Analyst for Goldman Sachs" },
     { name: "The Scott's Miracle Gro Company",
-        url: "../../../assets/images/scotts_miracle_gro.png",
+        url: "./../../../assets/images/scotts_miracle_gro.png",
         alttext: "The Scott's Miracle Gro Company",
         title: "Web Application Developer Intern, Summer 2017",
         location: "Marysville, OH",
@@ -810,7 +810,7 @@ var WORKDATA = [
             "Giving me new insite into buisness teamwork and working on core application backend and API functionality. " +
             "I also experienced my first buisness mistakes and trying to solve them, driving core buisness growth. " },
     { name: "IFAAADev",
-        url: "../../../assets/images/IFAADev.png",
+        url: "./../../../assets/images/IFAADev.png",
         alttext: "IFAADev",
         title: "Independent Contractor, Since May 2017",
         location: "Cleveland, OH",
@@ -821,7 +821,7 @@ var WORKDATA = [
             "This posed significant challengs using web API's as well as hosting services and frontend technologies. " +
             "I had to work as a buisness analyst for this project as well to collect requirments and demo the projeject to the client. " },
     { name: "Mayfield High School",
-        url: "../../../assets/images/mayfield_high.jpg",
+        url: "./../../../assets/images/mayfield_high.jpg",
         alttext: "Mayfield High School",
         title: "Class of 2014",
         location: "Mayfield Village, OH",
