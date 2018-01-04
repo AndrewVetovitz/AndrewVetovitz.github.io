f198c1c0c7797e3c68c31c3edee0210f061f21c3
AndrewVetovitz <AndrewVetovitz@gmail.com>
2018-01-04 12:48:52 -0500

    replaced dancer png with svg

--- src/app/components/projects/project-data.ts
+++ src/app/components/projects/project-data.ts
@@ -2,7 +2,7 @@ import { ProjectData } from './project-data.class';
 
 export const PROJECTDATA: ProjectData[] = [
   { name: 'Mystery and Romance are Gone', link: 'https://github.com/AndrewVetovitz/mysteryandromancearegone',
-      source: 'http://res.cloudinary.com/dg25vxfyl/image/upload/q_100/v1515087308/dancer_emwixo.png' },
+      source: 'http://res.cloudinary.com/dg25vxfyl/image/upload/q_100/v1515088089/dancer_ffarbv.svg' },
   { name: 'QuadTree Imaging', link: 'https://github.com/AndrewVetovitz/QuadTreeImaging',
       source: 'https://res.cloudinary.com/dg25vxfyl/image/upload/q_100/v1515014567/apple_aqcjn0.png' },
   { name: 'Personal Website', link: 'https://github.com/AndrewVetovitz/andrewvetovitz.github.io-personal-website',
